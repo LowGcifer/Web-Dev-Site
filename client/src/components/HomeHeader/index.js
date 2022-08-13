21fa1650ce1f3730742280eb8514728e9e3bf34a
LowGcifer <LowGcifer@gmail.com>
2022-08-13 00:11:13 -0400

    added text to home image

--- client/src/components/HomeHeader/index.js
+++ client/src/components/HomeHeader/index.js
@@ -3,7 +3,15 @@ import "../../style.css";
 
 function HomeHeader() {
   return (
-    <div id="desktop-header">{/* <img src={Logo} alt="Home Header" /> */}</div>
+    <div id="desktop-header">
+      {/* <img
+        src="../../images/My-Website-Home-Header.jpg"
+        alt="An open laptop lays on a black surface"
+        style="height:952px;"
+      ></img> */}
+      <p id="header-header-text">Logan Isley</p>
+      <p id="header-subheader-text">Full-Stack Web Developer</p>
+    </div>
   );
 }
 
