77495b7167fef0530134a83445c59ac8a5496e62
LowGcifer <LowGcifer@gmail.com>
2022-08-13 00:22:02 -0400

    started lead on home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -8,6 +8,16 @@ function Home() {
     <div>
       <Navbar></Navbar>
       <HomeHeader></HomeHeader>
+      <div className="home-lead">
+        <p id="home-lead-head">
+          It's time to up your web presence to compete <br></br> in the 21st
+          Century
+        </p>
+        <p id="home-lead-subhead">
+          A custom-coded website just for <br></br> your business is a necessity{" "}
+          <br></br>to make your brand stand out.
+        </p>
+      </div>
     </div>
   );
 }
