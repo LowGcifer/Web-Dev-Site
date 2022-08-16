131de49b45b32001222465c30da1638867733c08
LowGcifer <LowGcifer@gmail.com>
2022-08-16 02:19:23 -0400

    added two sections to home, home is now fully scrollable. Fix navbar not sticking to top when overflow-x: hidden is on

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -20,11 +20,24 @@ function Home() {
       </div>
       <div>
         <div id="home-image-one"></div>
-        <p></p>
+        <p id="home-image-paragraph-one">
+          {" "}
+          Work with me to bring Web <br></br>Designs to life and establish{" "}
+          <br></br>a unique, custom web presence <br></br>that'll be above your
+          competition
+        </p>
       </div>
       <div>
         <div id="home-image-two"></div>
+        <p id="home-image-paragraph-two">
+          Or start from scratch and work<br></br>with highly qualified web
+          designers<br></br>who can create the design of your<br></br>brand's
+          dreams.
+        </p>
       </div>
+
+      <div id="callout-home"></div>
+      <div id="home-final-section"></div>
     </div>
   );
 }
