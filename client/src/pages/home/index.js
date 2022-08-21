64f5ace002d33d8af76af46fa670747b6b713438
LowGcifer <LowGcifer@gmail.com>
2022-08-21 00:08:09 -0400

    got navbar to stick with overflow-x as hidden

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -7,46 +7,47 @@ function Home() {
   return (
     <div class="default-color">
       <Navbar></Navbar>
-      <HomeHeader></HomeHeader>
-      <div className="home-lead">
-        <p id="home-lead-head">
-          {" "}
-          <span class="lead-span">
-            It's time to up your web presence to compete <br></br> in the 21st
-            Century
-          </span>
-        </p>
-        <p id="home-lead-subhead">
-          <span class="lead-span">
-            A custom-coded website just for <br></br> your business is a
-            necessity <br></br>to make your brand stand out.
-          </span>
-        </p>
-      </div>
-      <div>
-        <div id="home-image-one"></div>
-        <p id="home-image-paragraph-one">
-          <span>
+      <div class="home-body">
+        <HomeHeader></HomeHeader>
+        <div className="home-lead">
+          <p id="home-lead-head">
             {" "}
-            Work with me to bring Web <br></br>Designs to life and establish{" "}
-            <br></br>a unique, custom web presence <br></br>that'll be above
-            your competition
-          </span>
-        </p>
+            <span class="lead-span">
+              It's time to up your web presence to compete <br></br> in the 21st
+              Century
+            </span>
+          </p>
+          <p id="home-lead-subhead">
+            <span class="lead-span">
+              A custom-coded website just for <br></br> your business is a
+              necessity <br></br>to make your brand stand out.
+            </span>
+          </p>
+        </div>
+        <div>
+          <div id="home-image-one"></div>
+          <p id="home-image-paragraph-one">
+            <span>
+              {" "}
+              Work with me to bring Web <br></br>Designs to life and establish{" "}
+              <br></br>a unique, custom web presence <br></br>that'll be above
+              your competition
+            </span>
+          </p>
+        </div>
+        <div>
+          <div id="home-image-two"></div>
+          <p id="home-image-paragraph-two">
+            <span>
+              Or start from scratch and work<br></br>with highly qualified web
+              designers<br></br>who can create the design of your<br></br>
+              brand's dreams.
+            </span>
+          </p>
+        </div>
+        <div id="callout-home"></div>
+        <div id="home-final-section"></div>
       </div>
-      <div>
-        <div id="home-image-two"></div>
-        <p id="home-image-paragraph-two">
-          <span>
-            Or start from scratch and work<br></br>with highly qualified web
-            designers<br></br>who can create the design of your<br></br>brand's
-            dreams.
-          </span>
-        </p>
-      </div>
-
-      <div id="callout-home"></div>
-      <div id="home-final-section"></div>
     </div>
   );
 }
