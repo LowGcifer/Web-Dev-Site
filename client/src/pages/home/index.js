0e8f480f8a555deb335e97c5c5b41f083d3f637f
LowGcifer <LowGcifer@gmail.com>
2022-08-21 00:02:02 -0400

    added inner border to home lead and font sizes

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -5,34 +5,43 @@ import "../../style.css";
 
 function Home() {
   return (
-    <div>
+    <div class="default-color">
       <Navbar></Navbar>
       <HomeHeader></HomeHeader>
       <div className="home-lead">
         <p id="home-lead-head">
-          It's time to up your web presence to compete <br></br> in the 21st
-          Century
+          {" "}
+          <span class="lead-span">
+            It's time to up your web presence to compete <br></br> in the 21st
+            Century
+          </span>
         </p>
         <p id="home-lead-subhead">
-          A custom-coded website just for <br></br> your business is a necessity{" "}
-          <br></br>to make your brand stand out.
+          <span class="lead-span">
+            A custom-coded website just for <br></br> your business is a
+            necessity <br></br>to make your brand stand out.
+          </span>
         </p>
       </div>
       <div>
         <div id="home-image-one"></div>
         <p id="home-image-paragraph-one">
-          {" "}
-          Work with me to bring Web <br></br>Designs to life and establish{" "}
-          <br></br>a unique, custom web presence <br></br>that'll be above your
-          competition
+          <span>
+            {" "}
+            Work with me to bring Web <br></br>Designs to life and establish{" "}
+            <br></br>a unique, custom web presence <br></br>that'll be above
+            your competition
+          </span>
         </p>
       </div>
       <div>
         <div id="home-image-two"></div>
         <p id="home-image-paragraph-two">
-          Or start from scratch and work<br></br>with highly qualified web
-          designers<br></br>who can create the design of your<br></br>brand's
-          dreams.
+          <span>
+            Or start from scratch and work<br></br>with highly qualified web
+            designers<br></br>who can create the design of your<br></br>brand's
+            dreams.
+          </span>
         </p>
       </div>
 
