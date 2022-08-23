a3dd1522f50d41bf99613e68d1bda81bfb566da0
LowGcifer <LowGcifer@gmail.com>
2022-08-23 01:39:35 -0400

    laid out most text in home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -24,29 +24,44 @@ function Home() {
             </span>
           </p>
         </div>
-        <div>
-          <div id="home-image-one"></div>
-          <p id="home-image-paragraph-one">
-            <span>
-              {" "}
-              Work with me to bring Web <br></br>Designs to life and establish{" "}
-              <br></br>a unique, custom web presence <br></br>that'll be above
-              your competition
-            </span>
+        <div id="home-image-area">
+          <div>
+            <div id="home-image-one"></div>
+            <p id="home-image-paragraph-one">
+              <span>
+                {" "}
+                Work with me to bring Web <br></br>Designs to life and establish{" "}
+                <br></br>a unique, custom web presence <br></br>that'll be above
+                your competition
+              </span>
+            </p>
+          </div>
+          <div>
+            <div id="home-image-two"></div>
+            <p id="home-image-paragraph-two">
+              <span>
+                Or start from scratch and work<br></br>with highly qualified web
+                designers<br></br>who can create the design of your<br></br>
+                brand's dreams.
+              </span>
+            </p>
+          </div>
+        </div>
+        <div id="callout-home">
+          <p id="callout-text">
+            Anyone can use the same WordPress theme that 1000 other<br></br>
+            businesses are using. In the age of the internet, having a<br></br>
+            unique, standout web presence is essential in crafting a brand
+            <br></br>that stands apart from your competitors.
           </p>
         </div>
-        <div>
-          <div id="home-image-two"></div>
-          <p id="home-image-paragraph-two">
-            <span>
-              Or start from scratch and work<br></br>with highly qualified web
-              designers<br></br>who can create the design of your<br></br>
-              brand's dreams.
-            </span>
+        <div id="home-final-section">
+          <p id="home-final-text">
+            Hi, I'm Logan Isley, I'm a Columbia-Certified Full-Stack Web
+            Developer,<br></br>and I'm going to bring your website to the 21st
+            Century.
           </p>
         </div>
-        <div id="callout-home"></div>
-        <div id="home-final-section"></div>
       </div>
     </div>
   );
