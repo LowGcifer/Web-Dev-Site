0d2fe1a80d68dc196e6afbc3871b65dced2ea4dd
LowGcifer <LowGcifer@gmail.com>
2022-08-25 00:08:45 -0400

    started next section on services

--- client/src/pages/services/index.js
+++ client/src/pages/services/index.js
@@ -5,11 +5,12 @@ function Services() {
   return (
     <div className="default-color">
       <Navbar></Navbar>
-      <div>
+      <div className="services-body">
         <div>
           <div id="services-header-image"></div>
           <h1 id="services-header-text">Services</h1>
         </div>
+        <div id="services-paths"></div>
       </div>
     </div>
   );
