1ede51ac29bf5ebff2d23e0a8639bd55e37d4969
LowGcifer <LowGcifer@gmail.com>
2022-08-25 03:34:52 -0400

    did some line stuff but mainly edited README

--- client/src/pages/services/index.js
+++ client/src/pages/services/index.js
@@ -10,7 +10,16 @@ function Services() {
           <div id="services-header-image"></div>
           <h1 id="services-header-text">Services</h1>
         </div>
-        <div id="services-paths"></div>
+        <div id="services-paths">
+          <div id="services-paths-head">
+            <p>
+              There are two starting points<br></br>for building your website:
+            </p>
+          </div>
+          <div id="services-paths-top-line">
+            <hr></hr>
+          </div>
+        </div>
         <div id="services-cost"></div>
         <div id="services-question-section"></div>
       </div>
