920c295a505d31c62cd5e3c25ee7aa9c546b6624
LowGcifer <LowGcifer@gmail.com>
2022-09-14 20:41:46 -0400

    started about page

--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -2,10 +2,27 @@ import Navbar from "../../components/Navbar";
 
 function About() {
   return (
-    <div>
+    <div className="default-color">
       <Navbar></Navbar>
-      <div>
-        <p>About</p>
+      <div id="about-body">
+        <h1 id="about-header">Here's Me</h1>
+        <div id="about-row-one">
+          <p id="about-paragraph-one">
+            Hi! I'm Logan Isley. I'm a web developer <br></br>and a passionate
+            guitarist! In 2021, I got <br></br>my certification in Full Stack
+            Web <br></br>Development from Columbia University.
+          </p>
+          <div id="about-image-one"></div>
+        </div>
+        <div id="about-row-two">
+          <p id="about-paragraph-two">
+            Outside of web development, you'll <br></br>find me making music,
+            hanging out with <br></br>friends, and playing video games! I've
+            always <br></br>had a knack for all things computer, so <br></br>
+            coding was a very obvious path for me to <br></br>go down, and here
+            I am today! Making <br></br>websites for people!
+          </p>
+        </div>
       </div>
     </div>
   );
