d523db9ac94cae18f264ba726f9a1e8d97b38f11
LowGcifer <LowGcifer@gmail.com>
2022-09-29 01:34:37 -0400

    express.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
-const express = require('express'); 
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get ('/express_backend' (req, res) => {
-    res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED"})
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
