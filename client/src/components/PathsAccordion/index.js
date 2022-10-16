d471b39dc071ccce3fee7433d7b052bb845cab21
LowGcifer <LowGcifer@gmail.com>
2022-10-16 13:34:59 -0400

    services page mobile refactoring

--- client/src/components/PathsAccordion/index.js
+++ client/src/components/PathsAccordion/index.js
@@ -3,6 +3,18 @@ import "./pathsaccordion.css";
 import { useState } from "react";
 
 function PathsAccordion() {
+  const pathsSections = [
+    {
+      title: "You already have a design:",
+      content:
+        "This means either you or a web designer have already created a web design, now you just need someone to turn that design into a real website.",
+    },
+    {
+      title: "You're starting from scratch:",
+      content: "This means you have nothing.",
+    },
+  ];
+
   return (
     <div>
       <p>Hello World!</p>
