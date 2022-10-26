a679a0c00e26b0c2ac4920d8e5a420a717d84fb9
LowGcifer <LowGcifer@gmail.com>
2022-10-26 01:59:00 -0400

    more accordion css

--- client/src/utils/content.js
+++ client/src/utils/content.js
@@ -5,10 +5,32 @@ export const pathsSections = [
       web designer have already created a web design, now you 
       just need someone to turn 
       that design into a real 
-      website.`,
+      website.
+      
+      
+      Great! We'll take a look at 
+      your design and talk details.
+      We'll come up with a quote
+      and a turnaround date, and 
+      I'll be ready to go.`,
   },
   {
     title: "You're starting from scratch:",
-    content: `This means you have nothing.`,
+    content: `This means you have nothing.
+    You don't have a design for 
+    your new website. This
+    means you'll not only have
+    a new website developed,
+    but also have one designed
+    
+    You'll either work with me or 
+    with my superb connections at
+    929 Studios to get a web design
+    that matches you and your brand.
+    
+    After you have a web design from
+    me or 929 Studios, I'll provide an 
+    estimate for development and begin
+    work!`,
   },
 ];
