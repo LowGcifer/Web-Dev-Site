6bddf765643835bf0281ee8517a75e7c05ba3fd7
LowGcifer <LowGcifer@gmail.com>
2022-11-06 23:25:25 -0500

    text realignment and refactoring on home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -13,19 +13,21 @@ function Home() {
       <div class="home-body">
         <HomeHeader></HomeHeader>
         <div className="home-lead">
-          <p id="home-lead-head">
-            {" "}
-            <span class="lead-span">
-              It's time to up your web presence to compete <br></br> in the 21st
-              Century
-            </span>
-          </p>
-          <p id="home-lead-subhead">
-            <span class="lead-span">
-              A custom-coded website just for <br></br> your business is a
-              necessity <br></br>to make your brand stand out.
-            </span>
-          </p>
+          <div id="home-lead-text">
+            <p id="home-lead-head">
+              {" "}
+              <span class="lead-span">
+                It's time to up your web presence to compete <br></br> in the
+                21st Century
+              </span>
+            </p>
+            <p id="home-lead-subhead">
+              <span class="lead-span">
+                A custom-coded website just for <br></br> your business is a
+                necessity <br></br>to make your brand stand out.
+              </span>
+            </p>
+          </div>
         </div>
         <div id="home-image-area">
           <div id="home-image-one-container">
