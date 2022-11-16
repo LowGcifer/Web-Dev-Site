0f49864458f655c5fda68d2ee69a48e49bff9a2c
LowGcifer <LowGcifer@gmail.com>
2022-11-16 14:32:10 -0500

    hamburger styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -78,12 +78,12 @@ function Navbar() {
             .navbar-custom ul {
               display: ${isNavExpanded ? "flex" : "none"};
               position: absolute;
-              top: 43px;
+              top: 30px;
               right: 0;
               padding-right: 15px;
               flex-direction: column;
               width: 100vw;
-              height: 100vh;
+              height: 22.5vh;
               background-color: black;
               border-top: 2px solid #3f58ac;
               justify-content: right;
