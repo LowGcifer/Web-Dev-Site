352f913e7d1959b811150f87db231a6847aba9f3
LowGcifer <LowGcifer@gmail.com>
2022-12-01 02:25:45 -0500

    contact

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,113 +1,63 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import "./contactform.css";
-
-// export default () => {
-//   const [status, setStatus] = useState({
-//     submitted: false,
-//     submitting: false,
-//     info: { error: false, msg: null },
-//   });
-//   const [inputs, setInputs] = useState({
-//     email: "",
-//     message: "",
-//   });
-//   const handleServerResponse = (ok, msg) => {
-//     if (ok) {
-//       setStatus({
-//         submitted: true,
-//         submitting: false,
-//         info: { error: false, msg: msg },
-//       });
-//       setInputs({
-//         email: "",
-//         message: "",
-//       });
-//     } else {
-//       setStatus({
-//         info: { error: true, msg: msg },
-//       });
-//     }
-//   };
-//   const handleOnChange = (e) => {
-//     e.persist();
-//     setInputs((prev) => ({
-//       ...prev,
-//       [e.target.id]: e.target.value,
-//     }));
-//     setStatus({
-//       submitted: false,
-//       submitting: false,
-//       info: { error: false, msg: null },
-//     });
-//   };
-//   const handleOnSubmit = (e) => {
-//     e.preventDefault();
-//     setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
-//     axios({
-//       method: "POST",
-//       url: "https://formspree.io/https://formspree.io/f/xbjbvkvr",
-//       data: inputs,
-//     })
-//       .then((response) => {
-//         handleServerResponse(
-//           true,
-//           "Thank you, your message has been submitted."
-//         );
-//       })
-//       .catch((error) => {
-//         handleServerResponse(false, error.response.data.error);
-//       });
-//   };
-//   return (
-//     <main>
-//       <h1>Or Send an Email!</h1>
-//       <hr />
-//       <form onSubmit={handleOnSubmit}>
-//         <label htmlFor="email">Your Email</label>
-//         <input
-//           id="email"
-//           type="email"
-//           name="_replyto"
-//           onChange={handleOnChange}
-//           required
-//           value={inputs.email}
-//         />
-//         <label htmlFor="message">Your Message</label>
-//         <textarea
-//           id="message"
-//           name="message"
-//           onChange={handleOnChange}
-//           required
-//           value={inputs.message}
-//         />
-//         <button type="submit" disabled={status.submitting}>
-//           {!status.submitting
-//             ? !status.submitted
-//               ? "Submit"
-//               : "Submitted"
-//             : "Submitting..."}
-//         </button>
-//       </form>
-//       {status.info.error && (
-//         <div className="error">Error: {status.info.msg}</div>
-//       )}
-//       {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
-//     </main>
-//   );
-// };
-
 import React from "react";
 import "./contactform.css";
 import { useForm, ValidationError } from "@formspree/react";
 function ContactForm() {
   const [state, handleSubmit] = useForm("xbjbvkvr");
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return <p>Thank you for your email! You will hear back from me soon!</p>;
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email Address</label>
+    <div className="contact-form-area">
+      <form
+        id="fs-frm"
+        name="simple-contact-form"
+        accept-charset="utf-8"
+        action="https://formspree.io/f/https://formspree.io/f/xbjbvkvr"
+        method="post"
+      >
+        <fieldset id="fs-frm-inputs">
+          <label for="full-name">Full Name</label>
+          <input
+            type="text"
+            name="name"
+            id="full-name"
+            placeholder="First and Last"
+            required=""
+          />
+          <label for="email-address">Email Address</label>
+          <input
+            type="email"
+            name="_replyto"
+            id="email-address"
+            placeholder="email@domain.tld"
+            required=""
+          />
+          <label for="message">Message</label>
+          <textarea
+            rows="5"
+            name="message"
+            id="message"
+            placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus."
+            required=""
+          ></textarea>
+          <input
+            type="hidden"
+            name="_subject"
+            id="email-subject"
+            value="Contact Form Submission"
+          />
+        </fieldset>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
+
+{
+  /* <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Your Email Address</label>
       <input id="email" type="email" name="email" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <textarea id="message" name="message" />
@@ -115,8 +65,5 @@ function ContactForm() {
       <button type="submit" disabled={state.submitting}>
         Submit
       </button>
-    </form>
-  );
+    </form> */
 }
-
-export default ContactForm;
--- client/src/pages/contact/index.js
+++ client/src/pages/contact/index.js
@@ -17,9 +17,8 @@ function Contact() {
             If you are interested in working with me, please schedule <br></br>{" "}
             a Zoom call with me using the calendar below. <br></br> If timezones
             or some other scheduling conflict prevent you <br></br> from
-            scheduling a Zoom call with me, then please email me <br></br> at{" "}
-            <a href="mailto:logan@929studios.com">logan@929studios.com</a>.
-            Thank you!
+            scheduling a Zoom call with me, then please email me <br></br>
+            using the contact form below. Thank you!
           </p>
         </div>
         <Calendly></Calendly>
