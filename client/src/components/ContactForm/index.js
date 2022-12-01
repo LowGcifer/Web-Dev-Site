1800dc9a8394c514e840a0bd645b7a2b1f323263
LowGcifer <LowGcifer@gmail.com>
2022-12-01 02:04:23 -0500

    contact

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,98 +1,122 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./contactform.css";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import "./contactform.css";
 
-export default () => {
-  const [status, setStatus] = useState({
-    submitted: false,
-    submitting: false,
-    info: { error: false, msg: null },
-  });
-  const [inputs, setInputs] = useState({
-    email: "",
-    message: "",
-  });
-  const handleServerResponse = (ok, msg) => {
-    if (ok) {
-      setStatus({
-        submitted: true,
-        submitting: false,
-        info: { error: false, msg: msg },
-      });
-      setInputs({
-        email: "",
-        message: "",
-      });
-    } else {
-      setStatus({
-        info: { error: true, msg: msg },
-      });
-    }
-  };
-  const handleOnChange = (e) => {
-    e.persist();
-    setInputs((prev) => ({
-      ...prev,
-      [e.target.id]: e.target.value,
-    }));
-    setStatus({
-      submitted: false,
-      submitting: false,
-      info: { error: false, msg: null },
-    });
-  };
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
-    axios({
-      method: "POST",
-      url: "https://formspree.io/https://formspree.io/f/xbjbvkvr",
-      data: inputs,
-    })
-      .then((response) => {
-        handleServerResponse(
-          true,
-          "Thank you, your message has been submitted."
-        );
-      })
-      .catch((error) => {
-        handleServerResponse(false, error.response.data.error);
-      });
-  };
+// export default () => {
+//   const [status, setStatus] = useState({
+//     submitted: false,
+//     submitting: false,
+//     info: { error: false, msg: null },
+//   });
+//   const [inputs, setInputs] = useState({
+//     email: "",
+//     message: "",
+//   });
+//   const handleServerResponse = (ok, msg) => {
+//     if (ok) {
+//       setStatus({
+//         submitted: true,
+//         submitting: false,
+//         info: { error: false, msg: msg },
+//       });
+//       setInputs({
+//         email: "",
+//         message: "",
+//       });
+//     } else {
+//       setStatus({
+//         info: { error: true, msg: msg },
+//       });
+//     }
+//   };
+//   const handleOnChange = (e) => {
+//     e.persist();
+//     setInputs((prev) => ({
+//       ...prev,
+//       [e.target.id]: e.target.value,
+//     }));
+//     setStatus({
+//       submitted: false,
+//       submitting: false,
+//       info: { error: false, msg: null },
+//     });
+//   };
+//   const handleOnSubmit = (e) => {
+//     e.preventDefault();
+//     setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+//     axios({
+//       method: "POST",
+//       url: "https://formspree.io/https://formspree.io/f/xbjbvkvr",
+//       data: inputs,
+//     })
+//       .then((response) => {
+//         handleServerResponse(
+//           true,
+//           "Thank you, your message has been submitted."
+//         );
+//       })
+//       .catch((error) => {
+//         handleServerResponse(false, error.response.data.error);
+//       });
+//   };
+//   return (
+//     <main>
+//       <h1>Or Send an Email!</h1>
+//       <hr />
+//       <form onSubmit={handleOnSubmit}>
+//         <label htmlFor="email">Your Email</label>
+//         <input
+//           id="email"
+//           type="email"
+//           name="_replyto"
+//           onChange={handleOnChange}
+//           required
+//           value={inputs.email}
+//         />
+//         <label htmlFor="message">Your Message</label>
+//         <textarea
+//           id="message"
+//           name="message"
+//           onChange={handleOnChange}
+//           required
+//           value={inputs.message}
+//         />
+//         <button type="submit" disabled={status.submitting}>
+//           {!status.submitting
+//             ? !status.submitted
+//               ? "Submit"
+//               : "Submitted"
+//             : "Submitting..."}
+//         </button>
+//       </form>
+//       {status.info.error && (
+//         <div className="error">Error: {status.info.msg}</div>
+//       )}
+//       {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
+//     </main>
+//   );
+// };
+
+import React from "react";
+import "./contactform.css";
+import { useForm, ValidationError } from "@formspree/react";
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xbjbvkvr");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
-    <main>
-      <h1>Or Send an Email!</h1>
-      <hr />
-      <form onSubmit={handleOnSubmit}>
-        <label htmlFor="email">Your Email</label>
-        <input
-          id="email"
-          type="email"
-          name="_replyto"
-          onChange={handleOnChange}
-          required
-          value={inputs.email}
-        />
-        <label htmlFor="message">Your Message</label>
-        <textarea
-          id="message"
-          name="message"
-          onChange={handleOnChange}
-          required
-          value={inputs.message}
-        />
-        <button type="submit" disabled={status.submitting}>
-          {!status.submitting
-            ? !status.submitted
-              ? "Submit"
-              : "Submitted"
-            : "Submitting..."}
-        </button>
-      </form>
-      {status.info.error && (
-        <div className="error">Error: {status.info.msg}</div>
-      )}
-      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
-    </main>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" name="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <textarea id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
   );
-};
+}
+
+export default ContactForm;
